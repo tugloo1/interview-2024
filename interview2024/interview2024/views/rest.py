f5a1cb56bc0c2476fd81d6546014b0d96225d9ae
Piyush Gupta <piyushg91@gmail.com>
2024-09-18 15:01:44 -0700

    Interview-commmit

--- interview2024/interview2024/views/rest.py
+++ interview2024/interview2024/views/rest.py
@@ -1,4 +1,5 @@
 from pyramid.response import Response
+import random
 from pyramid import request
 from pyramid.view import view_config
 from pyramid.view import view_defaults
@@ -6,6 +7,10 @@ import transaction
 
 
 from .. import models
+
+queue = {}
+job_id = 123
+
 
 
 @view_defaults(route_name='rest')
@@ -16,19 +21,33 @@ class RESTView(object):
 
     @view_config(request_method='GET')
     def get(self):
-        query = self.request.dbsession.query(models.MyModel)
-        one = query.filter(models.MyModel.name == 'one').one()
-        print(one.name)
-        print(one.value)
-        # return Response(json_body={'name': 'Piyush'})
-        return Response(json_body={'name': one.name, 'value': one.value})
+        body = self.request.json_body
+        job_id = body['job_id']
+        print(queue)
+        if job_id in queue:
+            return Response(json_body=queue[job_id])
+        return Response(json_body={})
 
     @view_config(request_method='POST')
     def post(self):
         print(self.request.json_body)
-        newModel = models.MyModel(name='two', value=2)
-        self.dbsession.add(newModel)
-        transaction.commit()
+        # TODO Validate all params there
+        job_id = random.randint(0, 10)
+        new_data = {'python_script': self.request.json_body['python_script'],
+                    'queue_name': self.request.json_body['queue_name'],
+                    'date_submitted': '20240918',
+                    'status': 'SUBMITTED',
+                    'time_submitted':  '2:39 PM PST',
+                    }
+        queue[job_id] = new_data
+        new_data['success'] = 'true '
+        new_data['job_id'] = job_id
+        print(queue)
+        return Response(json_body=new_data)
+
+        # newModel = models.MyModel(name='two', value=2)
+        # self.dbsession.add(newModel)
+        # transaction.commit()
         return Response(json_body={'success': 'true'})
 
     @view_config(request_method='PUT')
@@ -41,4 +60,11 @@ class RESTView(object):
 
     @view_config(request_method='DELETE')
     def delete(self):
-        return Response('delete')
+        body = self.request.json_body
+        job_id = body['job_id']
+        print(queue)
+        if job_id in queue:
+            del queue[job_id]
+            print(queue)
+            return Response(json_body={'success': 'true', 'date_deleted': 'YYYYMMDD', 'time_deleted': 'TODO: get time'})
+        return Response(json_body={'success': 'false'})
